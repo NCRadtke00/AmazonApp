5386a71bab8aceaf31dd819e9936815899abb741
Nicholas Radtke <Raddnc@gmail.com>
2021-02-15 01:45:52 -0600

    installed raect-router, working on app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
+import React from "react";
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    <h1>building amazon clone</h1>
-    </div>
-    
-     /* we need react-router */
-
-     /* localhost.com/ */ 
-     /* localhost.com/checkout */ 
-    /* localhost.com/login */ 
+    <Router>
+      <div className="app">
+        <Switch>
+          <Router path="/checkout">
+            <h1>Checkout</h1>
+          </Router>
+          <Router path="/login">
+            <h1>Login page</h1>
+          </Router>
+        </Switch>
+      </div>
+    </Router>
 
   );
 }
 
 export default App;
  
+     /* we need react-router */
+
+
+     /* localhost.com/ */ 
+     
+    
+    
+    
+     /* localhost.com/checkout */ 
+    
+    
+    
+     /* localhost.com/login */ 
