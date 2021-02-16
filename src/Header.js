2fb1cb3029dfdd2f0bc6a9ad0fac0d12157c0b0d
Nicholas Radtke <Raddnc@gmail.com>
2021-02-16 01:47:05 -0600

    Finished setting up react router, working on Header or navbar component

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            
+
+            //////logo on left
+            //////search box
+            /////3links
+            /////basket icon with number
+            
+        </div>
+    )
+}
+
+export default Header
