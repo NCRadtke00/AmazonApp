c8f8024495b1554d3b5496500b5d2650ad9d10e7
Nicholas Radtke <Raddnc@gmail.com>
2021-02-16 16:16:21 -0600

    cleaned up product component, styling will need some minor adjustments
    not shrinking correctly

--- src/Product.js
+++ src/Product.js
@@ -5,16 +5,16 @@ function Product({ id, title, image, price, rating }) {
     return (
         <div className="product">
             <div className="product__info">
-                   <p>{title}</p>
-            <p className="product__price">
+                <p>{title}</p>
+                <p className="product__price">
                 <small>$</small>
                 <strong> { price } </strong>
-            </p>
-            <div className="product__rating">
+                </p>
+                <div className="product__rating">
                 { Array(rating).fill().map( (_) => (
                     <p>⭐</p>
                 ))}
-            </div>
+                </div>
             </div>
           
             <img className="product__img"src={image} alt=""/>
