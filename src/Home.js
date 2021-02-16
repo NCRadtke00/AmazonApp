4d046266f0fe69f493261bf9774baf21c6a095aa
Nicholas Radtke <Raddnc@gmail.com>
2021-02-16 13:38:02 -0600

    setting up product component by declaring what the variables will be

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
     return (
         <div className="home">
-            <img src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._BC428684220_.jpg" class="home__image" alt=""/>
+            <img src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._BC428684220_.jpg" className="home__image" alt=""/>
+        
+        <Product 
+            id="12321341" 
+            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback" 
+            price={11.96} 
+            rating={5} 
+            image="https://images-eu.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"/>
+        
         </div>
     )
 }
