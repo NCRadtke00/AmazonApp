641949076c68a5c6de0fb4640aa577ec87b6ae54
Nicholas Radtke <Raddnc@gmail.com>
2021-02-16 17:22:14 -0600

    added logic to index.js for reducer and imported it

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StateProvider } from './StateProvider';
+import reducer, { initialState } from './Reducer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <StateProvider>
-    {/* initialState= {} reducer= {}  */}
+    <StateProvider initialState= {initialState} reducer= {reducer}>
+    {/*   */}
     <App /> 
     </StateProvider>
   </React.StrictMode>,
