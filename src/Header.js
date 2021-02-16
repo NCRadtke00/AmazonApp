7de3b3495e9c73e54724d186a4710f85bd9e3e02
Nicholas Radtke <Raddnc@gmail.com>
2021-02-16 02:47:13 -0600

    added shopping basket icon and number of items in basket to navbar

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Header.css";
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
     return (
@@ -18,25 +19,29 @@ function Header() {
             <div className="header__nav">
                 <Link to="/login" className="header__link">
                     <div className="header__option">
-                        <span>Hello,</span>
-                        <span>Sign In</span>
+                        <span className="header__optionLineOne">Hello,</span>
+                        <span className="header__optionLineTwo">Sign In</span>
                     </div>
                 </Link>
                 <Link to="/" className="header__link">
                     <div className="header__option">
-                        <span>Returns</span>
-                        <span>& Orders</span>
+                        <span className="header__optionLineOne">Returns</span>
+                        <span className="header__optionLineTwo">& Orders</span>
                     </div>
                 </Link>
                 <Link to="/" className="header__link">
                     <div className="header__option">
-                        <span>Your</span>
-                        <span>Prime</span>
+                        <span className="header__optionLineOne">Your</span>
+                        <span className="header__optionLineTwo">Prime</span>
                     </div>
                 </Link>
 
 
-                <Link>
+                <Link to="/checkout" className="header__link">
+                    <div className="header__optionBasket">
+                        <ShoppingBasketIcon/>
+                        <span>0</span>
+                    </div>
                 </Link>
             </div>
         </nav>
