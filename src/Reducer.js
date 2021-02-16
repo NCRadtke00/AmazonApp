2647e395a2639e5aa572f44ddd33399a55567879
Nicholas Radtke <Raddnc@gmail.com>
2021-02-16 17:21:49 -0600

    adding reducer for adding and removing items from the shopping basket

--- src/Reducer.js
+++ src/Reducer.js
@@ -1,3 +1,17 @@
 export const initialState = {
     basket: [],
 };
+
+function reducer(state, action) {
+    switch(action.type) {
+        case 'ADD_TO_BASKET':
+            //log to add to basket
+            break;
+        case 'REMOVE_FROM_BASKET':
+            //logic for removing from basket
+            break;
+        default:
+            return state;
+    }
+}
+export default reducer;
