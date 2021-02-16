4c921f68f579c2fe9a95553a6a367acf6509e635
Nicholas Radtke <Raddnc@gmail.com>
2021-02-16 02:34:56 -0600

    added 3 links next to search bar on the amazon navbar

--- src/Header.js
+++ src/Header.js
@@ -10,13 +10,35 @@ function Header() {
             <Link to="/">
             <img className="header__logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt=""/>
             </Link>
-            
-            
+        
             <div className="header__search">
                  <input type="text" className="header__searchInput" />
             <SearchIcon className="header__searchIcon" />
             </div>
+            <div className="header__nav">
+                <Link to="/login" className="header__link">
+                    <div className="header__option">
+                        <span>Hello,</span>
+                        <span>Sign In</span>
+                    </div>
+                </Link>
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span>Returns</span>
+                        <span>& Orders</span>
+                    </div>
+                </Link>
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span>Your</span>
+                        <span>Prime</span>
+                    </div>
+                </Link>
 
+
+                <Link>
+                </Link>
+            </div>
         </nav>
     )
 }
