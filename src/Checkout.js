eb1db66a48fa8d734c045b833aa0c4aa33ac432b
Nicholas Radtke <Raddnc@gmail.com>
2021-02-23 12:48:21 -0600

    created checkout.js, imported usestate, checkoutproduct and style sheet
    checkoutproduct.js and checkout.css havnt been created yet

---
+++ src/Checkout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useStateValue } from './StateProvider';
+import CheckoutProduct from './CheckoutProduct';
+import './Checkout.css';
+
+function Checkout() {
+    const [{basket}, dispatch] = useStateValue();
+    
+    return (
+
+    )
+}
+
+export default Checkout
