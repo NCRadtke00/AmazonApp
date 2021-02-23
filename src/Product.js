218563d991e3cb9624c89342175b1e43521d4f2f
Nicholas Radtke <Raddnc@gmail.com>
2021-02-23 11:55:15 -0600

    imported useStateValue's to product.js,
    setting basket action and item details when item is added to basket

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import './Product.css';
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
+    const [{ }, dispatch] = useStateValue();
+    const addToBasket = () => {
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating
+            }
+        })
+    };
+    
     return (
         <div className="product">
             <div className="product__info">
