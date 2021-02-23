b6ceadebcf668043ab1d0afb2abac29cc8e0211f
Nicholas Radtke <Raddnc@gmail.com>
2021-02-23 12:51:27 -0600

    added a log to track the item in console, aded temp item to style basket

--- src/Reducer.js
+++ src/Reducer.js
@@ -1,15 +1,26 @@
 export const initialState = {
-    basket: [],
+    basket: [{
+        id:"3254354345",
+        title: "Maledan Compatible with Fitbit Sense & Versa 3 Charger Replacement USB Charging Cable Dock Stand for Sense Smartwatch, 2 Pack 3.3Ft Durable Portable Charger Dock Power Cable Cord, Black/Black",
+        price: 9.99,
+        rating:4,
+        image:"https://images-na.ssl-images-amazon.com/images/I/71r6OE-WsTL._AC_SL1500_.jpg",
+    }],
+    user: null,
 };
 
 function reducer(state, action) {
-    switch(action.type) {
+    console.log(action);
+    switch (action.type) {
         case 'ADD_TO_BASKET':
             //log to add to basket
-            break;
+            return {
+                ...state,
+                basket: [...state.basket, action.item],
+            };
         case 'REMOVE_FROM_BASKET':
             //logic for removing from basket
-            break;
+            return {state};
         default:
             return state;
     }
