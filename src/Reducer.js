e217d1b460f6ce58972ea0431bdb10149572cc16
Nicholas Radtke <Raddnc@gmail.com>
2021-02-24 20:01:26 -0600

    added logic to remove items from shopping cart

--- src/Reducer.js
+++ src/Reducer.js
@@ -27,9 +27,26 @@ function reducer(state, action) {
             };
         case 'REMOVE_FROM_BASKET':
             //logic for removing from basket
-            return {state};
+            //cloned the basket
+            let newBasket = [...state.basket];
+
+            const index = state.basket.findIndex((basketItem) => basketItem.id === action.id);
+            
+            if(index >= 0){
+                //item exists in basket, remove it
+                newBasket.splice(index, 1);
+            } else {
+                console.warn(
+                'Can not remove product (id: ${action.id}) as it is not defined'
+                );
+            }
+            return {
+                ...state,
+                basket: newBasket,
+            };
         default:
             return state;
     }
-}
+};
+
 export default reducer;
