c0436ee516f75561b35bc4e6c2f0df58e3166283
Nicholas Radtke <Raddnc@gmail.com>
2021-02-25 15:34:05 -0600

    added function to login component and importing firebase usehistory and usestate

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Login.css";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "./firebase";
 
 function Login() {
+    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const signIn = e => {
+        e.preventDefault();
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then((auth) => {
+                history.push('/')
+            })
+        .catch (error => alert(error.message))
+    }
+    const register = e => {
+        e.preventDefault();
+
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                // created new user if this shows
+                console.log(auth);
+                if (auth) {
+                    history.push('/')
+                }
+            })
+            .catch(error => alert(error.message))
+    }
     return (
         <div className="login">
             <Link to='/'>
@@ -12,15 +39,17 @@ function Login() {
                 <h1>Sign-in</h1>
                 <form>
                     <h5>E-mail</h5>
-                    <input type="text" />
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+
                     <h5>Password</h5>
-                    <input type="text" />
-                    <button className="login__signInButton ">Sign In</button>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+
+                    <button type="submit" onClick={signIn} className="login__signInButton ">Sign In</button>
                 </form>
                 <p>
                     By signing-in you agree to the Amazon Clone Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interset-Based Ads Notice.
                 </p>
-                <button className="login__registerButton ">Create your Amazon Account</button>
+                <button onClick={register} className="login__registerButton ">Create your Amazon Account</button>
             </div>
        </div>
     )
