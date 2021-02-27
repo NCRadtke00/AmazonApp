da9b24933e294faa96634937864c84bf904fc059
Nicholas Radtke <Raddnc@gmail.com>
2021-02-27 15:03:37 -0600

    added usehistory from react-router-dom to send user to payment page, keeps styling in check

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,8 +3,10 @@ import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
 import { getBasketTotal } from "./Reducer";
+import { useHistory } from "react-router-dom";
 
 function Subtotal() {
+    const history = useHistory();
     const [{basket}, dispatch] = useStateValue();
 
     return (
@@ -28,7 +30,7 @@ function Subtotal() {
                 prefix={"$"}
             />
             {/* link button to checkoutpage  */}
-         <button>Proceed to Checkout</button>
+         <button onClick={e => history.push('/payment')}>Proceed to Checkout</button>
         </div>
     );
 }
