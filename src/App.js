ceeb60684396d089ff36669dbb4a8f7d1e138bf0
Nicholas Radtke <Raddnc@gmail.com>
2021-02-27 23:24:20 -0600

    finished setting app orderflow

--- src/App.js
+++ src/App.js
@@ -1,53 +1,67 @@
-import React, {useEffect} from "react";
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
 import Payment from "./Payment";
+import Orders from "./Orders";
 import { auth } from "./firebase";
-import { useStateValue } from './StateProvider';
+import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
+const promise = loadStripe(
+  "pk_test_51Hn9DtFfxvFC7Iq0iyfNbG2W1WpvsrknMUcCIzwJ2DgWYEV1vMQInmx4inXyacbqE6WsXzScNYLZE2JUbWks4nwQ00owATw32J"
+);
 
 function App() {
-  const [{ }, dispatch] = useStateValue();
-  
-  useEffect(() => {    
-    auth.onAuthStateChanged(authUser => {
-      console.log('THE USER IS >>>', authUser);
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
       if (authUser) {
-        
-        dispatch({
-          type: 'SET_USER',
-          user: authUser
-        })
+        // the user just logged in / the user was logged in
 
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
       } else {
+        // the user is logged out
         dispatch({
-          type: 'SET_USER',
-          user: null
-        })
+          type: "SET_USER",
+          user: null,
+        });
       }
-    })
-
-  }, [])
+    });
+  }, []);
 
   return (
     <Router>
       <div className="app">
         <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
             <Header />
             <Checkout />
           </Route>
-          <Route path="/login">
-            <Header />
-            <Login />
-          </Route>
           <Route path="/payment">
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
@@ -56,22 +70,7 @@ function App() {
         </Switch>
       </div>
     </Router>
-
   );
 }
 
 export default App;
- 
-     /* we need react-router */
-
-
-     /* localhost.com/ */ 
-     
-    
-    
-    
-     /* localhost.com/checkout */ 
-    
-    
-    
-     /* localhost.com/login */ 
